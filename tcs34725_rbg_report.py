95fcb359ec197a7a069bf15a385f6d687e573170
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2019-01-02 14:44:50 -0500

    Update tcs34725_rbg_report.py

--- tcs34725_rbg_report.py
+++ tcs34725_rbg_report.py
@@ -1,13 +1,9 @@
 import smbus
 import time
 bus = smbus.SMBus(1)
-# I2C address 0x29
-# Register 0x12 has device ver.
-# Register addresses must be OR'ed with 0x80
 bus.write_byte(0x29,0x80|0x12)
 ver = bus.read_byte(0x29)
 if ver == 0x44:
- print "Device found\n"
  bus.write_byte(0x29, 0x80|0x00) # 0x00 = ENABLE register
  bus.write_byte(0x29, 0x01|0x02) # 0x01 = Power on, 0x02 RGB sensors enabled
  bus.write_byte(0x29, 0x80|0x14) # Reading results start register 14, LSB then MSB
@@ -22,11 +18,10 @@ if ver == 0x44:
   green = (green/256)
   blue = (blue/256)
   crgb = "C: %s, R: %s, G: %s, B: %s\n" % (clear, red, green, blue)
-  print crgb
+  #print crgb
   if red > ((green*1.2)+1) and red > ((blue*1.2)+1):
 	print "On"
   else:
 	print "off"
-  time.sleep(.2)
 else:
  print "Device not found\n"
